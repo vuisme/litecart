755ded892f3fdfa7a1d211404e3030a23fe85625
cpanel10x <me@vutn.net>
2024-10-10 11:55:19 +0700

    Update page.go

--- internal/handlers/private/page.go
+++ internal/handlers/private/page.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"github.com/gofiber/fiber/v2"
 
-	"github.com/shurco/litecart/internal/models"
-	"github.com/shurco/litecart/internal/queries"
-	"github.com/shurco/litecart/pkg/logging"
-	"github.com/shurco/litecart/pkg/webutil"
+	"github.com/vuisme/litecart/internal/models"
+	"github.com/vuisme/litecart/internal/queries"
+	"github.com/vuisme/litecart/pkg/logging"
+	"github.com/vuisme/litecart/pkg/webutil"
 )
 
 // Pages is ...
